15441e328f12c22921364908b2c4b7d79a16316c
ryry <resnonmotus@gmail.com>
2023-11-20 06:56:08 +0800

    add tests

---
+++ test/README.md
@@ -0,0 +1,12 @@
+### PamilyHelper e2e testing
+
+
+#### Getting started
+
+0. Make sure `PamilyHelper` webapp is running.
+
+1. Install cypress with: `npm install cypress`
+
+2. Run `npm run cypress:open`
+
+3. Run e2e tests.
---
+++ test/cypress/e2e/index/navigation.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+context("Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5000");
+  });
+
+  it("Index page should contain the navigation", () => {
+    cy.get(".navbar").contains("PamilyaHelper").click();
+    cy.get("#navbarCollapse").contains("Home");
+    cy.get("#navbarCollapse").contains("About");
+    cy.get("#navbarCollapse").contains("Job List");
+    cy.get("#navbarCollapse").contains("Helpers");
+    cy.get("#navbarCollapse").contains("Contact");
+    cy.get("#navbarCollapse").contains("Login/SignUp");
+  });
+
+  it("Home navigation should go to /", () => {
+    cy.get("#navbarCollapse > div > a.nav-item.nav-link.active")
+      .contains("Home")
+      .click();
+    cy.get(".navbar").contains("PamilyaHelper").click();
+  });
+
+  it("About navigation should go to /about", () => {
+    cy.get("#navbarCollapse > div > a.nav-item.nav-link")
+      .contains("About")
+      .click();
+    cy.location("pathname").should("include", "about");
+  });
+
+  it("cy.reload() - reload the page", () => {
+    cy.reload();
+    cy.reload(true); // reload the page without using the cache
+  });
+});
